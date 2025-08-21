ff6bc2d29912b213e2f9bdfe3aab985dbcffa158
FormAToz <kycok2@yandex.ru>
2025-08-21 09:12:23 +0300

    [gw-server] Get 'preferred_username' from auth token

--- gw-server/src/main/java/ru/format/demo/gw_server/filter/TrackingFilter.java
+++ gw-server/src/main/java/ru/format/demo/gw_server/filter/TrackingFilter.java
@@ -2,9 +2,14 @@ package ru.format.demo.gw_server.filter;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -21,6 +26,7 @@ public class TrackingFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        log.debug("The authentication name from the token is : {}", getPreferredUsername(requestHeaders));
         exchange = filterUtils.setCorrelationId(exchange, getCorrelationId(requestHeaders));
         return chain.filter(exchange);
     }
@@ -40,5 +46,27 @@ public class TrackingFilter implements GlobalFilter {
 
     private String generateCorrelationId() {
         return UUID.randomUUID().toString();
+    }
+
+    @NonNull
+    private String getPreferredUsername(HttpHeaders requestHeaders) {
+        String authToken = filterUtils.getAuthToken(requestHeaders);
+        if (authToken == null) {
+            return StringUtils.EMPTY;
+        }
+
+        try {
+            JSONObject obj = decodeJwt(authToken);
+            return obj.getString("preferred_username");
+        } catch (JSONException e) {
+            log.error("Unable to get data from auth token", e);
+            return StringUtils.EMPTY;
+        }
+    }
+
+    private JSONObject decodeJwt(String authToken) {
+        String base64EncodedBody = authToken.split("\\.")[1];
+        String body = new String(new Base64(true).decode(base64EncodedBody));
+        return new JSONObject(body);
     }
 }
